68c835baf3790063f03a0c809a55225192261614
Mr-Raja12 <rk9576899082@gmail.com>
2022-12-26 00:53:13 +0530

    Added : User Need to check Valid Last Name

--- src/main/java/com/bl/junitprogram/UserRegistration.java
+++ src/main/java/com/bl/junitprogram/UserRegistration.java
@@ -1,4 +1,5 @@
 package com.bl.junitprogram;
+
 import org.junit.Test;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -9,14 +10,22 @@ import java.util.regex.Pattern;
  *
  */
 public class UserRegistration {
-	
-    @Test
-    public boolean firstName(String firstName) {
-        String regex = "^[A-Z]{1}[a-z]{2,}$";
-        //create a pattern
-        Pattern pattern = Pattern.compile(regex);
-        //get a matcher object
-        Matcher matcher = pattern.matcher(firstName);
-        return matcher.matches();
-    }
+
+	@Test
+	public boolean firstName(String firstName) {
+		String regex = "^[A-Z]{1}[a-z]{2,}$";
+		// create a pattern
+		Pattern pattern = Pattern.compile(regex);
+		// get a matcher object
+		Matcher matcher = pattern.matcher(firstName);
+		return matcher.matches();
+	}
+
+	@Test
+	public boolean lastName(String lastname) {
+		String regex = "^[A-Z]{1}[a-z]{2,}$";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(lastname);
+		return matcher.matches();
+	}
 }
--- src/main/java/com/bl/junitprogram/UserRegistrationTest.java
+++ src/main/java/com/bl/junitprogram/UserRegistrationTest.java
@@ -1,6 +1,5 @@
 package com.bl.junitprogram;
 
-
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -10,22 +9,33 @@ import org.junit.Test;
  *
  */
 public class UserRegistrationTest {
-	 UserRegistration userRegistration = new UserRegistration();
-	 
-        /*
-         * User need to a valid first name
-         */
-	    @Test
-	    public void givenFirstName_WhenProper_ShouldReturnTrue() {
-	        boolean result = userRegistration.firstName("Raja");
-	        Assert.assertEquals(true, result);
-	    }
+	UserRegistration userRegistration = new UserRegistration();
 
-	    @Test
-	    public void givenFirstName_WhenNotProper_ShouldReturnFalse() {
-	        boolean result = userRegistration.firstName("raja");
-	        Assert.assertEquals(false, result);
-	    }
+	/*
+	 * User need to a valid first name, last name
+	 */
+	@Test
+	public void givenFirstName_WhenProper_ShouldReturnTrue() {
+		boolean result = userRegistration.firstName("Raja");
+		Assert.assertEquals(true, result);
+	}
+
+	@Test
+	public void givenFirstName_WhenNotProper_ShouldReturnFalse() {
+		boolean result = userRegistration.firstName("raja");
+		Assert.assertEquals(false, result);
+	}
+
+	@Test
+	public void givenLastName_whenProper_ShouldReturnTrue() {
+		boolean result = userRegistration.lastName("Baitha");
+		Assert.assertEquals(true, result);
+	}
+
+	@Test
+	public void givenLastName_WhenNotProper_ShouldReturnFalse() {
+		boolean result = userRegistration.lastName("baitha");
+		Assert.assertEquals(false, result);
+	}
 
 }
-	
